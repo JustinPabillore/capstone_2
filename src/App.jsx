3f6ec685eddfcf9ed12417acf62adc77be33544d
Justin_Pabs <125973797+JustinPabillore@users.noreply.github.com>
2024-07-02 22:57:00 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-container">
+      <div className="top-container">
+        <div className="box">
+          <label className="box-label">CITC</label>
+        </div>
+        <div className="box">
+          <label className="box-label">COT</label>
+        </div>
+        <div className="box">
+          <label className="box-label">CSTE</label>
+        </div>
+        <div className="box">
+          <label className="box-label">COM</label>
+        </div>
+        <div className="box">
+          <label className="box-label">CSM</label>
+        </div>
+        <div className="box">
+          <label className="box-label">CEA</label>
+        </div>
+        <div className="box">
+          <label className="box-label">SHS</label>
+        </div>
+        <div className="box">
+          <label className="box-label">EMPLOYEE</label>
+        </div>
+        <div className="box">
+          <label className="box-label">VISITOR</label>
+        </div>
+        <div className="box">
+          <label className="box-label">TOTAL</label>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// #0b1933
+// #fcb414
