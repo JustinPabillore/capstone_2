a309d45f04fdfcb23e99ad92171dabf85e3bf051
Justin_Pabs <125973797+JustinPabillore@users.noreply.github.com>
2024-07-03 10:42:32 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,52 @@ function App() {
   return (
     <div className="main-container">
       <div className="top-container">
-        <div className="box">
-          <label className="box-label">CITC</label>
+        <div className="box1">
+          <label className="box1-label">CITC</label>
         </div>
-        <div className="box">
-          <label className="box-label">COT</label>
+        <div className="box1">
+          <label className="box1-label">COT</label>
         </div>
-        <div className="box">
-          <label className="box-label">CSTE</label>
+        <div className="box1">
+          <label className="box1-label">CSTE</label>
         </div>
-        <div className="box">
-          <label className="box-label">COM</label>
+        <div className="box1">
+          <label className="box1-label">COM</label>
         </div>
-        <div className="box">
-          <label className="box-label">CSM</label>
+        <div className="box1">
+          <label className="box1-label">CSM</label>
         </div>
-        <div className="box">
-          <label className="box-label">CEA</label>
+        <div className="box1">
+          <label className="box1-label">CEA</label>
         </div>
-        <div className="box">
-          <label className="box-label">SHS</label>
+        <div className="box1">
+          <label className="box1-label">SHS</label>
         </div>
-        <div className="box">
-          <label className="box-label">EMPLOYEE</label>
+        <div className="box1">
+          <label className="box1-label">EMPLOYEE</label>
         </div>
-        <div className="box">
-          <label className="box-label">VISITOR</label>
+        <div className="box1">
+          <label className="box1-label">VISITOR</label>
         </div>
-        <div className="box">
-          <label className="box-label">TOTAL</label>
+        <div className="box1">
+          <label className="box1-label">TOTAL</label>
+        </div>
+      </div>
+      <div className="mid1-container">
+        <span className="mid1-text">UNIVERSITY OF SCIENCE AND TECHNOLOGY OF SOUTHERN PHILIPPINES</span>
+      </div>
+      <div className="mid2-container">
+        <div className="box2">
+          <label className="box2-label">BOX 1</label>
+        </div>
+        <div className="box2">
+          <label className="box2-label">BOX 2</label>
+        </div>
+        <div className="box2">
+          <label className="box2-label">BOX 3</label>
+        </div>
+        <div className="box2">
+          <label className="box2-label">BOX 4</label>
         </div>
       </div>
     </div>
@@ -41,5 +58,3 @@ function App() {
 
 export default App;
 
-// #0b1933
-// #fcb414
