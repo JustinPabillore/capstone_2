18318d834d9147d9232a472774ff6a1f1ad7b91f
Justin_Pabs <125973797+JustinPabillore@users.noreply.github.com>
2024-07-06 21:29:50 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import View from './View';
 import './index.css';
+import '@fontsource/poppins'; // Default weight (400)
+import '@fontsource/poppins/700.css'; // Weight 800
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
