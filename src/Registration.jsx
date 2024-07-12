1ee3c31a8404c47d451caee2c65a1cce7369e611
Justin_Pabs <125973797+JustinPabillore@users.noreply.github.com>
2024-07-12 11:09:39 +0800

    .

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -4,6 +4,22 @@ import './Registration.css';
 function Registration() {
   return (
     <div className="main-container3">
+      <div className="top-container1">
+        <label className="main-title">VISITOR REGISTRATION</label>
+      </div>
+      <div className="mid1-container">
+        <span className="mid1-text">
+          UNIVERSITY OF SCIENCE AND TECHNOLOGY OF SOUTHERN PHILIPPINES MONITORING SYSTEM
+        </span>
+      </div>
+      <div className="bottom-container">
+        <div className="left-container">
+          {/* Add content for the left container here */}
+        </div>
+        <div className="right-container">
+          {/* Add content for the right container here */}
+        </div>
+      </div>
     </div>
   );
 }
